ab927995be2aa967fa7e27a7a169e8ecefff93c0
vaskir <vaskir@gmail.com>
2018-04-28 19:55:08 +0300

    init

---
+++ ConsoleApp4/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open FSharp.Configuration
+
+type Yaml = YamlConfig<YamlText = "foo: 22">
+
+[<EntryPoint>]
+let main argv =
+    let yaml = Yaml()
+    printfn "foo = %d" yaml.foo
+    
+    0
